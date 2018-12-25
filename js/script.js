b3daf98f1b686104640a87849222b06d6301103e
Margarita <margarita.antonovna@mail.ru>
2018-12-25 20:10:15 +0300

    на защиту

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 var button = document.querySelector(".button-to-popup");
-        var popup = document.querySelector(".popup");
-        var form = popup.querySelector(".form-search");
-        var inputdatein = form.querySelector("#date-in");
-        var inputdateout = form.querySelector("#date-out");
-        var inputcountmen = form.querySelector("#count-men");
-        var inputcountchildren = form.querySelector("#count-children");
-        
-        button.addEventListener("click", function (evt) {
-            evt.preventDefault();
-            popup.classList.toggle("popup-show");
+var popup = document.querySelector(".popup");
+var form = popup.querySelector(".form-search");
+var inputdatein = form.querySelector("#date-in");
+var inputdateout = form.querySelector("#date-out");
+var inputcountmen = form.querySelector("#count-men");
+var inputcountchildren = form.querySelector("#count-children");
+
+button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("popup-show");
   });
-        form.addEventListener("submit", function (evt) {
-    if (!inputdatein.value || !inputdateout.value || !inputcountmen.value || !inputcountchildren.value) {
-      evt.preventDefault();
-        popup.classList.remove("popup-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("popup-error");   
+    form.addEventListener("submit", function (evt) {
+        if (!inputdatein.value || !inputdateout.value || !inputcountmen.value || !inputcountchildren.value) {
+            evt.preventDefault();
+            popup.classList.remove("popup-error");
+            popup.offsetWidth = popup.offsetWidth;
+            popup.classList.add("popup-error");   
   }});
